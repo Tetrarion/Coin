958f072327bf243260438d40f6046aae57bafb9e
Tetrarion <mr.gilnich@gmail.com>
2022-04-05 19:02:19 +0300

    transfer proxy-server files in a new folder and update package.json

--- server.js
+++ server.js
@@ -1,55 +1,12 @@
-import getInfo from './API/api.js';
 import express from 'express';
-import { graphqlHTTP } from 'express-graphql';
-import { schema } from './schema.js';
-import cors from 'cors';
-import getFixedHistory from './utilities/getHistory.js';
-import { sortCoins } from './utilities/sortCoins.js';
+import path from 'path';
 
-const root = {
-  getCoin: async ({ id }) => {
-    const responce = await getInfo(`assets/${id}`);
-    return responce;
-  },
-  getCurrentCoins: async ({ firstIndex, coinsPerPage }) => {
-    const responce = await getInfo(`assets?offset=${firstIndex}&limit=${coinsPerPage}`);
-    return responce;
-  },
-  getRate: async ({ id }) => {
-    const responce = await getInfo(`rates/${id}`);
-    return responce;
-  },
-  getAllRates: async () => {
-    const responce = await getInfo(`rates`);
-    return responce;
-  },
-  getHistory: async ({ id, interval }) => {
-    let responce = await getInfo(`assets/${id}/history?interval=${interval}`);
-    responce = getFixedHistory(responce, interval);
-    return responce;
-  },
-  getCurrentSearchedCoins: async ({ search, firstIndex, coinsPerPage }) => {
-    const responce = await getInfo(`assets?search=${search}&offset=${firstIndex}&limit=${coinsPerPage}`);
-    return responce;
-  },
-  getSearchedCoins: async ({ search }) => {
-    const responce = await getInfo(`assets?search=${search}&limit=2000`);
-    return responce;
-  },
-  getCurrentSortedCoins: async ({ sortingName, firstIndex, coinsPerPage }) => {
-    let responce = await getInfo('assets?limit=2000');
-    responce = sortCoins(sortingName, responce, firstIndex, coinsPerPage);
-    return responce;
-  },
-};
+const port = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(cors());
+app.use(express.static(path.join('./client/', "build")));
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  rootValue: root,
-  graphiql: true,
-}));
-app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
+app.get('/', (req, res) => res.sendFile(path.join('./client/', "build", "index.html")));
+
+app.listen(port, () => console.log(`Server listening on port localhost:${port}`));
